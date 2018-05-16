6acdf2b3b6a7d788932ce49b606e4ea68949d5b3
Cade Gillem <cade.p.gillem@wmich.edu>
2018-05-16 10:48:11 -0400

    Add serializer, router, and viewset for users resource.

--- lists/urls.py
+++ lists/urls.py
@@ -1,7 +1,13 @@
+from django.conf.urls import url, include
+from rest_framework import routers
 from django.urls import path
 
 from . import views
 
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path(r'', include(router.urls)),
 ]
--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,13 @@
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from lists.serializers import UserSerializer
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello")
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API Endpoint for Users.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
