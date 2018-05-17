446878f5d959bed65e169f0589970a28237a4867
Cade Gillem <cade.p.gillem@wmich.edu>
2018-05-17 14:28:26 -0400

    Make login view functional.

--- assets/js/components/Login.vue
+++ assets/js/components/Login.vue
@@ -1,10 +1,36 @@
 <template>
     <div>
-        login
+        <form @submit.prevent="login">
+            <label for="username">Username
+                <input id="username" name="username" type="text" v-model="username">
+            </label>
+            <label for="password">Password
+                <input id="password" name="password" type="password" v-model="password">
+            </label>
+            <input type="submit" value="Login">
+        </form>
     </div>
 </template>
 
 <script>
+import auth from '../auth';
+
 export default {
+    data() {
+        return {
+            username: '',
+            password: '',
+        }
+    },
+    methods: {
+        login() {
+            auth.login(this.username, this.password, res => {
+                if (res) {
+                    // If the login is successful, redirect to home.
+                    this.$router.push('/home');
+                }
+            })
+        }
+    }
 }
 </script>
