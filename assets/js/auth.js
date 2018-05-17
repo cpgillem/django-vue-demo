ac45523f204d705b2701915ca3d88130d4804238
Cade Gillem <cade.p.gillem@wmich.edu>
2018-05-17 14:08:34 -0400

    Install axios and implement login function.

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,6 +1,29 @@
-export default {
-    login (email, password) {
+import axios from 'axios';
 
+export default {
+    // Takes a username, password, and callback function. The callback function takes a boolean.
+    login (username, password, next) {
+        if (localStorage.token) {
+            // If the token already exists, send true to the callback.
+            next(true);
+        } else {
+            // If not, send a request for a new token.
+            axios.post('/rest-auth/login/', {
+                username,
+                password,
+            }).then(response => {
+                if (response.data.key) {
+                    // If the response has a key string, store that in localStorage.
+                    localStorage.token = response.data.key
+                    next(true);
+                } else {
+                    // If not, there has been an error.
+                    next(false);
+                }
+            }).catch(error => {
+                next(false);
+            });
+        }
     },
 
     getToken() {
