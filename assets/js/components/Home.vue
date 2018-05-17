4ae1545ccfea4012aad51c2fb3eaab8562327f13
Cade Gillem <cade.p.gillem@wmich.edu>
2018-05-17 14:38:31 -0400

    Implement fetchData method for Home component.

--- assets/js/components/Home.vue
+++ assets/js/components/Home.vue
@@ -1,10 +1,36 @@
 <template>
     <div>
-        home
+        {{ lists }}
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+import auth from '../auth';
+
 export default {
+    data() {
+        return {
+            lists: [],
+        };
+    },
+    created() {
+        this.fetchData();
+    },
+    watch: {
+        '$route': 'fetchData',
+    },
+    methods: {
+        fetchData() {
+            axios.get('/api/lists', {
+                headers: {
+                    'Authorization': 'Token ' + auth.getToken(),
+                }
+            }).then(res => {
+                // Once the lists are loaded, store them in the list variable.
+                this.lists = res.data;
+            });
+        }
+    }
 }
 </script>
