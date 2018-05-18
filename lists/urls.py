e58751dc9ac57011e9766d8b7698f4ea2048581e
Cade Gillem <cade.p.gillem@wmich.edu>
2018-05-18 12:28:01 -0400

    Fix url order.

--- lists/urls.py
+++ lists/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
 
     # API Views.
     url(r'api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'rest-auth/', include('rest_auth.urls')),
     url(r'rest-auth/registration', include('rest_auth.registration.urls')),
+    url(r'rest-auth/', include('rest_auth.urls')),
     path(r'api/', include(router.urls)),
 ]
