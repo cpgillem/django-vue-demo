ff821f122ecacc7e1347b66fb84a558f218d91f6
Cade Gillem <cade.p.gillem@wmich.edu>
2018-05-21 13:51:14 -0400

    Add styling to register view.

--- assets/js/components/Register.vue
+++ assets/js/components/Register.vue
@@ -1,20 +1,24 @@
 <template>
-    <div>
-        <form @submit.prevent="register">
-            <label for="username">Username
-                <input id="username" name="username" type="text" v-model="username">
-            </label>
-            <label for="email">Email
-                <input id="email" name="email" type="email" v-model="email">
-            </label>
-            <label for="password1">Password
-                <input id="password1" name="password1" type="password" v-model="password1">
-            </label>
-            <label for="password2">Confirm Password
-                <input id="password2" name="password2" type="password" v-model="password2">
-            </label>
-            <input type="submit" value="Register">
-        </form>
+    <div class="grid-container">
+        <div class="grid-x">
+            <div class="cell small-12 medium-6 medium-offset-3">
+                <form @submit.prevent="register">
+                    <label for="username">Username
+                        <input id="username" name="username" type="text" v-model="username">
+                    </label>
+                    <label for="email">Email
+                        <input id="email" name="email" type="email" v-model="email">
+                    </label>
+                    <label for="password1">Password
+                        <input id="password1" name="password1" type="password" v-model="password1">
+                    </label>
+                    <label for="password2">Confirm Password
+                        <input id="password2" name="password2" type="password" v-model="password2">
+                    </label>
+                    <input class="button" type="submit" value="Register">
+                </form>
+            </div>
+        </div>
     </div>
 </template>
 
