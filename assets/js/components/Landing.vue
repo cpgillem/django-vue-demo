3de95567c9f90a354b436d71d8e778659c475977
Cade Gillem <cade.p.gillem@wmich.edu>
2018-05-22 13:22:55 -0400

    Add text to landing page.

--- assets/js/components/Landing.vue
+++ assets/js/components/Landing.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="grid-container">
-        landing page
+        <h1>Django Vue Demo</h1>
+        <p>Welcome to my Django+Vue demo project. This is a simple webapp that demonstrates the use of a full set of web development tools. If you would like to learn how to set up the basic structure of this site, take a look at my <a href="https://github.com/cpgillem/django-vue-demo/wiki/Tutorial">tutorial</a>. The full source code for this project is on <a href="https://github.com/cpgillem/django-vue-demo">Github</a>. If you're just testing it out, get started by clicking on the "Register" link.</p>
+        <p><small>Developed by Cade Gillem (<a href="http://cadegillem.com">cadegillem.com</a>).</small></p>
     </div>
 </template>
 
