a230b29e75c87c02536da69ea8ef2ce36d8e6876
Cade Gillem <cade.p.gillem@wmich.edu>
2018-05-22 12:40:43 -0400

    Add custom object permissions.

---
+++ lists/permissions.py
@@ -0,0 +1,9 @@
+from lists.models import List, Item
+from rest_framework import permissions
+
+class IsOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, object):
+        if isinstance(object, List):
+            return object.owner == request.user.profile
+        elif isinstance(object, Item):
+            return object.list.owner == request.user.profile
